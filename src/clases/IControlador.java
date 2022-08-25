bf8837b4cfbabfcf779bbd8d24d80337243b3205
BraG29 <braianbarrios_29@hotmail.com>
2022-08-25 15:55:03 -0300

    Update IControlador

--- src/clases/IControlador.java
+++ src/clases/IControlador.java
@@ -4,11 +4,15 @@ import java.time.LocalDate;
 
 public class IControlador {
 
-	public void altaUsuario(
+	public boolean altaUsuario(
 			String nick, String nombre,String apellido,String email,LocalDate fechaNac
-			) {};
-	public void altaUsuario(
+			) {
+		return true;
+		}
+	public boolean altaUsuario(
 			String nick, String nombre,String apellido,String email,LocalDate fechaNac,
 			String institucion, String descripcion, String biografia, String sitioWeb
-			) {};
+			) {
+		return true;
+	}
 }
