88586cdd6219190d5b82b7e02bbf26063e38338f
ShodytheWolf <49129728+ShodytheWolf@users.noreply.github.com>
2022-08-27 20:00:17 -0300

    arreglando cagadas mias
    también hice una funcion para pasar de Date a LocalDateTime

--- src/gui/AltaActividadDeportiva.java
+++ src/gui/AltaActividadDeportiva.java
@@ -1,15 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package gui;
 
-/**
- *
- * @author progappvm
- */
+import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 public class AltaActividadDeportiva extends javax.swing.JFrame {
 
+    public LocalDateTime convertirALocalDateTime(Date dateAConvertir){
+        return dateAConvertir.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+    
     /**
      * Creates new form AltaActividadDeportiva
      */
@@ -34,9 +34,10 @@ public class AltaActividadDeportiva extends javax.swing.JFrame {
         fieldNombre = new javax.swing.JTextField();
         fieldDesc = new javax.swing.JTextField();
         fieldDuracion = new javax.swing.JTextField();
-        jSpinner1 = new javax.swing.JSpinner();
+        spinnerCosto = new javax.swing.JSpinner();
         jLabel3 = new javax.swing.JLabel();
-        jSpinner2 = new javax.swing.JSpinner();
+        spinnerFecha = new javax.swing.JSpinner();
+        jLabel4 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -62,6 +63,11 @@ public class AltaActividadDeportiva extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("Noto Sans", 1, 18)); // NOI18N
         jButton1.setText("Aceptar");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -117,19 +123,16 @@ public class AltaActividadDeportiva extends javax.swing.JFrame {
             }
         });
 
-        jSpinner1.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
+        spinnerCosto.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
 
         jLabel3.setText("Costo $");
 
-        jSpinner2.setModel(new javax.swing.SpinnerDateModel(new java.util.Date(1661233467001L), null, null, java.util.Calendar.DAY_OF_YEAR));
+        spinnerFecha.setModel(new javax.swing.SpinnerDateModel(new java.util.Date(1661233467001L), null, null, java.util.Calendar.DAY_OF_YEAR));
+
+        jLabel4.setText("MM/DD/AA");
 
         jButton2.setBackground(new java.awt.Color(255, 51, 51));
         jButton2.setText("<---------");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -142,18 +145,21 @@ public class AltaActividadDeportiva extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel1)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(spinnerCosto, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jLabel3))
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(6, 6, 6)
                                 .addComponent(jLabel2))
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(jSpinner2, javax.swing.GroupLayout.Alignment.LEADING)
                                 .addComponent(fieldDuracion, javax.swing.GroupLayout.Alignment.LEADING)
                                 .addComponent(fieldDesc, javax.swing.GroupLayout.Alignment.LEADING)
                                 .addComponent(fieldNombre, javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(fieldInstiDepo, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 227, Short.MAX_VALUE))))
+                                .addComponent(fieldInstiDepo, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 227, Short.MAX_VALUE)
+                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                    .addComponent(spinnerFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                    .addComponent(jLabel4)))))
                     .addComponent(jButton2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -174,10 +180,12 @@ public class AltaActividadDeportiva extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(fieldDuracion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(3, 3, 3)
-                .addComponent(jSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(spinnerFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(spinnerCosto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 31, Short.MAX_VALUE)
                 .addComponent(jButton2))
@@ -231,6 +239,26 @@ public class AltaActividadDeportiva extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_fieldDuracionMouseClicked
 
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        String nombreActividad = fieldNombre.getText();
+        String nombreInsti = fieldInstiDepo.getText();
+        String desc = fieldDesc.getText();
+        
+        try{
+            float dura = Float.parseFloat(fieldDuracion.getText());
+        }catch(Exception e){
+            VentanaMensaje errorVentana = new VentanaMensaje("ERROR!","caracteres invalidos en campo Duracion",java.awt.Color.RED);
+            errorVentana.setVisible(true);
+            return;
+        }
+        
+        LocalDateTime fechaAlta = convertirALocalDateTime(((Date)spinnerFecha.getValue()));
+        //System.out.println(fechaAlta);
+        
+        //altaActividadDepo(nombreActividad,nombreInsti,desc,dura,costo,fechaAlta);       pa cuando haya backend
+        
+    }//GEN-LAST:event_jButton1MouseClicked
+
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
@@ -280,8 +308,9 @@ public class AltaActividadDeportiva extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JSpinner jSpinner1;
-    private javax.swing.JSpinner jSpinner2;
+    private javax.swing.JSpinner spinnerCosto;
+    private javax.swing.JSpinner spinnerFecha;
     // End of variables declaration//GEN-END:variables
 }
